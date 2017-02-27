bf077bcf3c72fb99704d7bae272c7cdf6a1e07cd
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2017-02-27 11:18:07 +0200

    git basically working

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::UsersController < Api::V1::BaseController
   def show
     user = User.find(params[:id])
 
-    render jsonapi: user, serializer: Api::V1::UserSerializer
+    # render jsonapi: user, serializer: Api::V1::UserSerializer
+    render json: user, serializer: Api::V1::UserSerializer
   end
 end
